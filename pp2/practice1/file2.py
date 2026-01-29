4979ce75e8886fd918dfb4432e8f41bb6c1ac279
“amangeldiizhanerke” <ryskeldi78@gmail.com>
2026-01-29 23:15:16 +0500

    data types task

---
+++ pp2/practice1/file2.py
@@ -0,0 +1,15 @@
+n = int(input())
+lst = []
+lstnumbers=[]
+lstothers=[]
+for i in range(n):
+    x = input()
+    lst.append(x)
+    if x.isdigit() or (x[0]=="-" and x[1:].isdigit()):
+        x=int(x)
+        lstnumbers.append(x)
+    else:
+        lstothers.append(x)
+print ("general list:", lst)
+print ("integer numbers' list:", lstnumbers)
+print ("not integer list:", lstothers)
