d2f9b9f80be9a24192ac07ba2d307be6b1bfb46b
“amangeldiizhanerke” <ryskeldi78@gmail.com>
2026-02-06 00:22:02 +0500

    Add if statement examples

---
+++ Practice2/if_else/if_statement.py
@@ -0,0 +1,30 @@
+# Example 1:
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+# Example 2:
+number = 15
+if number > 0:
+  print("The number is positive")
+
+#Example 3 (Multiple statements in an if block:):
+age = 20
+if age >= 18:
+  print("You are an adult")
+  print("You can vote")
+  print("You have full legal rights")
+
+# Example 4 (Using Variables in Conditions):
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back!")
+
+"""
+To write an if statement in python:
+Used the word if
+Writen a condition
+Used a colon (:)
+The code is indented below so program understands what line links with the condition
+"""
