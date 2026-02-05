85ed893938decf0c1d7d35860032a53fe73192e9
“amangeldiizhanerke” <ryskeldi78@gmail.com>
2026-02-06 00:13:44 +0500

    Boolean intro example

---
+++ Practice2/boolean/boolean_intro.py
@@ -0,0 +1,16 @@
+#Example 1
+print(bool("Hello")) #Prints "True" as it is not empty
+print(bool(15)) #Prints "True" as it is not empty
+
+
+#Example 2
+x = "Hello" #Value of Variable x
+y = 15 #Value of Variable y
+print(bool(x)) #Prints "True" as it is not empty
+print(bool(y)) #Prints "True" as it is not empty
+
+"""
+in Python bool can check whether it is empty or not,
+if it is empty program prints False, if it is not empty
+program prints True
+"""
