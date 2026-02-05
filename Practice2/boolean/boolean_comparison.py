e1467064ee53f8f22344db38e44ee5b8a7aba214
“amangeldiizhanerke” <ryskeldi78@gmail.com>
2026-02-05 23:30:23 +0500

    Boolean comparison examples

--- Practice2/boolean/boolean_comparison.py
+++ Practice2/boolean/boolean_comparison.py
@@ -1,17 +1,17 @@
 # Example 1
-print(10 > 9)   #Compares 10 and 9, prints True
-print(10 == 9)  #Compares 10 and 9, prints False
-print(10 < 9)   #Compares 10 and 9, prints False
+print(10 > 9) #Compares 10 and 9, prints True
+print(10 == 9) #Compares 10 and 9, prints False
+print(10 < 9) #Compares 10 and 9, prints False
 
 
 # Example 2
-a = 200  #Value of variable a
-b = 33   #Value of variable b
+a = 200 #Value of variable a
+b = 33 #Value of variable b
 
-if b > a:  #Checks if b is greater than a
-    print("b is greater than a")  #Printed if condition is True
+if b > a: #Checks if b is greater than a
+    print("b is greater than a") #Printed if condition is True
 else:
-    print("b is not greater than a")  #Printed as condition is False
+    print("b is not greater than a") #Printed as condition is False
 
 
 
