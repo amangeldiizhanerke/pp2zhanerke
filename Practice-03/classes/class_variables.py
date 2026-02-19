ea352042b2d9371e1c19c5a4905fb84affa040cf
“amangeldiizhanerke” <ryskeldi78@gmail.com>
2026-02-19 23:27:05 +0500

    Update class variables

--- Practice-03/classes/class_variables.py
+++ Practice-03/classes/class_variables.py
@@ -1,4 +1,13 @@
 class MyClass:
-  x = 5
+  x = 5   # class variable
+
 p1 = MyClass()
+p2 = MyClass()
+
 print(p1.x)
+print(p2.x)
+
+MyClass.x = 10   # change class variable
+
+print(p1.x)
+print(p2.x)
